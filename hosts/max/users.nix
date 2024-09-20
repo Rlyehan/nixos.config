11d4129e65c42dbb60d7ec5d0360a8e12bcf5b23
installer <installer@gmail.com>
2024-09-20 09:30:52 +0200

    initial commit

---
+++ hosts/max/users.nix
@@ -0,0 +1,29 @@
+{
+  pkgs,
+  username,
+  ...
+}:
+
+let
+  inherit (import ./variables.nix) gitUsername;
+in
+{
+  users.users = {
+    "${username}" = {
+      homeMode = "755";
+      isNormalUser = true;
+      description = "${gitUsername}";
+      extraGroups = [
+        "networkmanager"
+        "wheel"
+        "libvirtd"
+        "scanner"
+        "lp"
+      ];
+      shell = pkgs.bash;
+      ignoreShellProgramCheck = true;
+      packages = with pkgs; [
+      ];
+    };
+  };
+}
---
+++ hosts/max/variables.nix
@@ -0,0 +1,16 @@
+{
+  # Git Configuration ( For Pulling Software Repos )
+  gitUsername = "Rlyehan";
+  gitEmail = "maximilian.hub@proton.me";
+
+  # Hyprland Settings
+  extraMonitorSettings = "";
+
+  # Waybar Settings
+  clock24h = true;
+
+  # Program Options
+  browser = "brave";
+  terminal = "alacritty";
+  keyboardLayout = "us";
+}
