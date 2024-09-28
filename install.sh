1a733a70a1c77175cc98da33fc69ac46b7b65377
installer <installer@gmail.com>
2024-09-28 17:31:47 +0200

    updated script

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+if [ -n "$(grep -i nixos < /etc/os-release)" ]; then
+  echo "Verified this is NixOS."
+  echo "-----"
+else
+  echo "This is not NixOS or the distribution information is not available."
+  exit
+fi
+
+hostName="max"
+
+echo "Generating The Hardware Configuration"
+sudo nixos-generate-config --show-hardware-config > ./hosts/$hostName/hardware.nix
+
+echo "-----"
+
+echo "Setting Required Nix Settings Then Going To Install"
+NIX_CONFIG="experimental-features = nix-command flakes"
+
+echo "-----"
+
+sudo nixos-rebuild switch --flake ~/zaneyos/#${hostName}
