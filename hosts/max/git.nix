f2e3daf8b0f4e3212021277486c0038d3baa6c79
Maximilian Huber <maximilian.hub@proton.me>
2024-09-28 16:15:28 +0200

    further git config changes

--- hosts/max/git.nix
+++ hosts/max/git.nix
@@ -1,95 +1,96 @@
 { config, pkgs, ... }:
 
 {
-  programs.git = {
-    enable = true;
-    userName = "Maximilian Huber";
-    userEmail = "maximilian.hub@proton.me";
+programs.git = {
+  enable = true;
+  userName = "Maximilian Huber";
+  userEmail = "maximilian.hub@proton.me";
 
-    includes = [
-      {
-        condition = "gitdir:~/work/";
-        contents = {
-          user = {
-            email = "replace.me";
-          };
+  includes = [
+    {
+      condition = "gitdir:~/work/";
+      contents = {
+        user = {
+          email = "replace.me";  # Replace with your work email
         };
-      }
-    ];
-
-    extraConfig = {
-      github.user = "Rlyehan";
-
-      core = {
-        excludesfile = "~/.config/git/ignore";
-        filemode = false;
-        trustctime = false;
-        autocrlf = "input";
-        untrackedCache = true;
-        pager = "diff-so-fancy | less --tabs=4 -RFX";
-        ignorecase = false;
       };
+    }
+  ];
 
-      credential = {
-        helper = "libsecret";
+  extraConfig = {
+    github.user = "Rlyehan";
+
+    core = {
+      excludesfile = "~/.config/git/ignore";
+      filemode = false;
+      trustctime = false;
+      autocrlf = "input";
+      untrackedCache = true;
+      pager = "diff-so-fancy | less --tabs=4 -RFX";
+      ignorecase = false;
+    };
+
+    credential = {
+      helper = "libsecret";
+      "https://github.com".helper = "!/usr/bin/gh auth git-credential";
+      "https://gist.github.com".helper = "!/usr/bin/gh auth git-credential";
+    };
+
+    pull.rebase = true;
+    init.defaultBranch = "main";
+
+    push = {
+      default = "simple";
+      followTags = true;
+      autoSetupRemote = true;
+    };
+
+    fetch.prune = true;
+    grep.lineNumber = true;
+    help.autocorrect = 1;
+
+    diff = {
+      tool = "vimdiff";
+      algorithm = "histogram";
+    };
+
+    merge = {
+      conflictstyle = "diff3";
+      defaultToUpstream = true;
+    };
+
+    rebase.autoStash = true;
+
+    color = {
+      ui = true;
+      diff-highlight = {
+        oldNormal = "red bold";
+        oldHighlight = "red bold 52";
+        newNormal = "green bold";
+        newHighlight = "green bold 22";
       };
-
-      pull.rebase = true;
-      init.defaultBranch = "main";
-
-      push = {
-        default = "simple";
-        followTags = true;
-        autoSetupRemote = true;
-      };
-
-      fetch.prune = true;
-      grep.lineNumber = true;
-      help.autocorrect = 1;
-
       diff = {
-        colorMoved = "default";
-        algorithm = "histogram";
-      };
-
-      merge = {
-        conflictstyle = "diff3";
-        defaultToUpstream = true;
-      };
-
-      rebase.autoStash = true;
-
-      color = {
-        ui = "auto";
-        branch = {
-          current = "yellow reverse";
-          local = "yellow";
-          remote = "green";
-        };
-        diff = {
-          meta = "yellow bold";
-          frag = "magenta bold";
-          old = "red bold";
-          new = "green bold";
-        };
-        status = {
-          added = "yellow";
-          changed = "green";
-          untracked = "cyan";
-        };
-      };
-
-      interactive = {
-        diffFilter = "delta --color-only";
-      };
-
-      filter.lfs = {
-        clean = "git-lfs clean -- %f";
-        smudge = "git-lfs smudge -- %f";
-        process = "git-lfs filter-process";
-        required = true;
+        meta = "yellow";
+        frag = "magenta bold";
+        commit = "yellow bold";
+        old = "red bold";
+        new = "green bold";
+        whitespace = "red reverse";
       };
     };
+
+    interactive = {
+      diffFilter = "diff-so-fancy --patch";
+    };
+
+    filter.lfs = {
+      clean = "git-lfs clean -- %f";
+      smudge = "git-lfs smudge -- %f";
+      process = "git-lfs filter-process";
+      required = true;
+    };
+  };
+
 
     aliases = {
       aliases = "!git config --get-regexp alias | sed -re 's/alias\\.(\\S*)\\s(.*)$/\\1 = \\2/g'";
