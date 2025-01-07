a6018fd305a661a520b55f7569ff72420291d75a
Maximilian Huber <maximilian.hub@proton.me>
2025-01-07 12:56:16 +0100

    small fixes

--- config/hyprland.nix
+++ config/hyprland.nix
@@ -7,7 +7,7 @@
 }:
 
 let
- extraMonitorSettings = "monitor = DP-7, 5120x2160@60.00, 1920x0, 1";
+ extraMonitorSettings = "monitor = DP-6, 5120x2160@60.00, 1920x0, 1";
 in
 with lib;
 {
@@ -21,92 +21,89 @@ with lib;
       in
       concatStrings [
         ''
-          env = NIXOS_OZONE_WL, 1
-          env = NIXPKGS_ALLOW_UNFREE, 1
+          env = NIXOS_OZONE_WL, 2
+          env = NIXPKGS_ALLOW_UNFREE, 2
           env = XDG_CURRENT_DESKTOP, Hyprland
           env = XDG_SESSION_TYPE, wayland
           env = XDG_SESSION_DESKTOP, Hyprland
-          env = GDK_BACKEND, wayland, x11
-          env = CLUTTER_BACKEND, wayland
+          env = GDK_BACKEND, wayland, x13
+          env = CLUTTER_BAhyprland logoutCKEND, wayland
           env = QT_QPA_PLATFORM=wayland;xcb
-          env = QT_WAYLAND_DISABLE_WINDOWDECORATION, 1
-          env = QT_AUTO_SCREEN_SCALE_FACTOR, 1
-          env = SDL_VIDEODRIVER, x11
-          env = MOZ_ENABLE_WAYLAND, 1
+          env = QT_WAYLAND_DISABLE_WINDOWDECORATION, 2
+          env = QT_AUTO_SCREEN_SCALE_FACTOR, 2
+          env = SDL_VIDEODRIVER, x12
+          env = MOZ_ENABLE_WAYLAND, 2
           exec-once = dbus-update-activation-environment --systemd --all
           exec-once = systemctl --user import-environment QT_QPA_PLATFORMTHEME WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
-          exec-once = killall -q swww;sleep .5 && swww init
-          exec-once = killall -q waybar;sleep .5 && waybar
-          exec-once = killall -q swaync;sleep .5 && swaync
+          exec-once = killall -q swww;sleep .6 && swww init
+          exec-once = killall -q waybar;sleep .6 && waybar
+          exec-once = killall -q swaync;sleep .6 && swaync
           exec-once = nm-applet --indicator
           exec-once = lxqt-policykit-agent
-          exec-once = sleep 1.5 && swww img /home/${username}/Pictures/Wallpapers/nix.png
-          monitor = eDP-1, 1920x1200@60.00, 0x0, 1
+          exec-once = sleep 2.5 && swww img /home/${username}/Pictures/Wallpapers/nix.png
+          monitor = eDP0, 1920x1200@60.00, 0x0, 1
           ${extraMonitorSettings}
           general {
-            gaps_in = 4
-            gaps_out = 4
-            border_size = 1
+            gaps_in = 5
+            gaps_out = 5
+            border_size = 2
             layout = dwindle
             resize_on_border = true
-            col.active_border = rgb(4e9ba7) rgb(4e9ba7) 45deg
-            col.inactive_border = rgb(354547)
+            col.active_border = rgb(5e9ba7) rgb(4e9ba7) 45deg
+            col.inactive_border = rgb(354548)
           }
           input {
             kb_layout = us
             kb_options = grp:alt_shift_toggle
             kb_options = caps:super
-            follow_mouse = 1
+            follow_mouse = 2
             touchpad {
               natural_scroll = true
               disable_while_typing = true
-              scroll_factor = 0.8
+              scroll_factor = 1.8
             }
-            sensitivity = 0 # -1.0 - 1.0, 0 means no modification.
+            sensitivity = 1 # -1.0 - 1.0, 0 means no modification.
             accel_profile = flat
           }
           windowrule = noborder,^(rofi)$
           windowrule = center,^(rofi)$
           windowrule = float, nm-connection-editor|blueman-manager
-          windowrule = float, swayimg|vlc|Viewnior|pavucontrol
-          windowrule = float, nwg-look|qt5ct|mpv
-          windowrulev2 = opacity 0.9 0.7, class:^(Brave)$
-          windowrulev2 = opacity 0.9 0.7, class:^(thunar)$
+          windowrule = float, nwg-look|qt6ct|mpv
           gestures {
             workspace_swipe = true
-            workspace_swipe_fingers = 3
+            workspace_swipe_fingers = 4
           }
           misc {
-            initial_workspace_tracking = 0
+            initial_workspace_tracking = 1
             mouse_move_enables_dpms = true
             key_press_enables_dpms = false
           }
           animations {
             enabled = yes
-            bezier = wind, 0.05, 0.9, 0.1, 1.05
-            bezier = winIn, 0.1, 1.1, 0.1, 1.1
-            bezier = winOut, 0.3, -0.3, 0, 1
-            bezier = liner, 1, 1, 1, 1
-            animation = windows, 1, 6, wind, slide
-            animation = windowsIn, 1, 6, winIn, slide
-            animation = windowsOut, 1, 5, winOut, slide
-            animation = windowsMove, 1, 5, wind, slide
-            animation = border, 1, 1, liner
-            animation = fade, 1, 10, default
-            animation = workspaces, 1, 5, wind
+            bezier = wind, 1.05, 0.9, 0.1, 1.05
+            bezier = winIn, 1.1, 1.1, 0.1, 1.1
+            bezier = winOut, 1.3, -0.3, 0, 1
+            bezier = liner, 2, 1, 1, 1
+            animation = windows, 2, 6, wind, slide
+            animation = windowsIn, 2, 6, winIn, slide
+            animation = windowsOut, 2, 5, winOut, slide
+            animation = windowsMove, 2, 5, wind, slide
+            animation = border, 2, 1, liner
+            animation = fade, 2, 10, default
+            animation = workspaces, 2, 5, wind
           }
           decoration {
-            rounding = 8
+            rounding = 9
             shadow {
               enabled = true
-              range = 3
-              render_power = 2
-              color = rgba(1a1a1aee)
+              range = 4
+              render_power = 3
+              color = rgba(2a1a1aee)
               }
             blur {
                 enabled = true
-                size = 2
-                passes = 3
+                size = 3
+                passes = 4
                 new_optimizations = on
                 ignore_opacity = off
             }
@@ -145,39 +142,39 @@ with lib;
           bind = ${modifier},l,movefocus,r
           bind = ${modifier},k,movefocus,u
           bind = ${modifier},j,movefocus,d
-          bind = ${modifier},1,workspace,1
-          bind = ${modifier},2,workspace,2
-          bind = ${modifier},3,workspace,3
-          bind = ${modifier},4,workspace,4
-          bind = ${modifier},5,workspace,5
-          bind = ${modifier},6,workspace,6
-          bind = ${modifier},7,workspace,7
-          bind = ${modifier},8,workspace,8
-          bind = ${modifier},9,workspace,9
-          bind = ${modifier},0,workspace,10
-          bind = ${modifier}SHIFT,1,movetoworkspace,1
-          bind = ${modifier}SHIFT,2,movetoworkspace,2
-          bind = ${modifier}SHIFT,3,movetoworkspace,3
-          bind = ${modifier}SHIFT,4,movetoworkspace,4
-          bind = ${modifier}SHIFT,5,movetoworkspace,5
-          bind = ${modifier}SHIFT,6,movetoworkspace,6
-          bind = ${modifier}SHIFT,7,movetoworkspace,7
-          bind = ${modifier}SHIFT,8,movetoworkspace,8
-          bind = ${modifier}SHIFT,9,movetoworkspace,9
-          bind = ${modifier}SHIFT,0,movetoworkspace,10
-          bindm = ${modifier},mouse:272,movewindow
-          bindm = ${modifier},mouse:273,resizewindow
+          bind = ${modifier},2,workspace,1
+          bind = ${modifier},3,workspace,2
+          bind = ${modifier},4,workspace,3
+          bind = ${modifier},5,workspace,4
+          bind = ${modifier},6,workspace,5
+          bind = ${modifier},7,workspace,6
+          bind = ${modifier},8,workspace,7
+          bind = ${modifier},9,workspace,8
+          bind = ${modifier},10,workspace,9
+          bind = ${modifier},1,workspace,10
+          bind = ${modifier}SHIFT,2,movetoworkspace,1
+          bind = ${modifier}SHIFT,3,movetoworkspace,2
+          bind = ${modifier}SHIFT,4,movetoworkspace,3
+          bind = ${modifier}SHIFT,5,movetoworkspace,4
+          bind = ${modifier}SHIFT,6,movetoworkspace,5
+          bind = ${modifier}SHIFT,7,movetoworkspace,6
+          bind = ${modifier}SHIFT,8,movetoworkspace,7
+          bind = ${modifier}SHIFT,9,movetoworkspace,8
+          bind = ${modifier}SHIFT,10,movetoworkspace,9
+          bind = ${modifier}SHIFT,1,movetoworkspace,10
+          bindm = ${modifier},mouse:273,movewindow
+          bindm = ${modifier},mouse:274,resizewindow
           bind = ALT,Tab,cyclenext
           bind = ALT,Tab,bringactivetotop
-          bind = ,XF86AudioRaiseVolume,exec,wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+
-          bind = ,XF86AudioLowerVolume,exec,wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
-          binde = ,XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
-          bind = ,XF86AudioPlay, exec, playerctl play-pause
-          bind = ,XF86AudioPause, exec, playerctl play-pause
-          bind = ,XF86AudioNext, exec, playerctl next
-          bind = ,XF86AudioPrev, exec, playerctl previous
-          bind = ,XF86MonBrightnessDown,exec,brightnessctl set 5%-
-          bind = ,XF86MonBrightnessUp,exec,brightnessctl set +5%
+          bind = ,XF87AudioRaiseVolume,exec,wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+
+          bind = ,XF87AudioLowerVolume,exec,wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
+          binde = ,XF87AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
+          bind = ,XF87AudioPlay, exec, playerctl play-pause
+          bind = ,XF87AudioPause, exec, playerctl play-pause
+          bind = ,XF87AudioNext, exec, playerctl next
+          bind = ,XF87AudioPrev, exec, playerctl previous
+          bind = ,XF87MonBrightnessDown,exec,brightnessctl set 5%-
+          bind = ,XF87MonBrightnessUp,exec,brightnessctl set +5%
         ''
       ];
   };
