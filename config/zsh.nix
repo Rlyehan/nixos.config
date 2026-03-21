8d2e93f8dfd7b5168a324abd12a28b2ac4166fde
Maximilian Huber <maximilian.hub@proton.me>
2026-03-21 13:04:51 +0100

    alias update

--- config/zsh.nix
+++ config/zsh.nix
@@ -19,12 +19,12 @@
       # NixOS rebuild (current config, defaults to hyprland)
       fr = "nh os switch --hostname ${host} /home/${username}/nixos.config";
       fu = "nh os switch --hostname ${host} --update /home/${username}/nixos.config";
-      # Switch to Hyprland
-      fh = "nh os switch --hostname ${host}-hyprland /home/${username}/nixos.config";
-      fhu = "nh os switch --hostname ${host}-hyprland --update /home/${username}/nixos.config";
-      # Switch to COSMIC
-      fc = "nh os switch --hostname ${host}-cosmic /home/${username}/nixos.config";
-      fcu = "nh os switch --hostname ${host}-cosmic --update /home/${username}/nixos.config";
+      # Switch to Hyprland (boot — takes effect on next reboot)
+      fh = "nh os boot --hostname ${host}-hyprland /home/${username}/nixos.config";
+      fhu = "nh os boot --hostname ${host}-hyprland --update /home/${username}/nixos.config";
+      # Switch to COSMIC (boot — takes effect on next reboot)
+      fc = "nh os boot --hostname ${host}-cosmic /home/${username}/nixos.config";
+      fcu = "nh os boot --hostname ${host}-cosmic --update /home/${username}/nixos.config";
       # Garbage collection
       ncg = "nix-collect-garbage --delete-old && sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
       cat = "bat";
